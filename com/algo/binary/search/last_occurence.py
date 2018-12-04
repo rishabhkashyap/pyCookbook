80ea78e6817d2551fac22d56ab892e69fcae746e
Rishabh Kashyap <kashyap.rishab91@gmail.com>
2018-12-04 21:22:54 +0530

    First and last occurrence of element in sorted array

---
+++ com/algo/binary/search/last_occurence.py
@@ -0,0 +1,21 @@
+def last_ocurrence(arr, key):
+    low = 0
+    high = len(arr) - 1
+    result = -1
+    while (low <= high):
+        mid = (low + high) // 2
+        if (arr[mid] == key):
+            result = mid
+            low = mid + 1
+        elif (arr[mid] < key):
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return result
+
+
+if __name__ == '__main__':
+    arr = [2, 5, 5, 5, 6, 6, 8, 9, 9, 9]
+    result = last_ocurrence(arr, 5)
+    print("First occurrence  of 5 =  {}".format(result))
